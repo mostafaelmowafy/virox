c60fad935cd985ed2a6a0ac085cbb15702315c79
mostafaelmowafy <mostafaelmowafy1@gmail.com>
2026-04-27 02:22:19 +0300

    commit

--- components/FacebookPixel.tsx
+++ components/FacebookPixel.tsx
@@ -5,7 +5,7 @@ import { usePathname, useSearchParams } from 'next/navigation';
 import { useEffect } from 'react';
 
 // ضع رقم البيكسل الخاص بك هنا مباشرة بين العلامتين
-const FB_PIXEL_ID = '01234567890123'; // استبدل هذا بالرقم الحقيقي لبيكسل فيسبوك الخاص بك
+const FB_PIXEL_ID = '1880112229329412'; // استبدل هذا بالرقم الحقيقي لبيكسل فيسبوك الخاص بك
 
 export const FacebookPixel = () => {
   const pathname = usePathname();
